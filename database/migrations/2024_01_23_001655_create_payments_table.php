5a23599c1d5ae0db64caffa1bbd3b808ef8a0860
nurhabibrs <nurhabibrs@gmail.com>
2024-02-03 16:36:05 +0700

    Change id to uuid

--- database/migrations/2024_01_23_001655_create_payments_table.php
+++ database/migrations/2024_01_23_001655_create_payments_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('payments', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id');
             $table->string('name');
             $table->text('address');
             $table->string('phone');
